6fbda6b6dad0f9d5dcec87bd29d56c3020939c47
Florian Thiel <flth@flth.de>
2023-07-09 14:08:15 +0200

    initial

---
+++ mod.lua
@@ -0,0 +1,27 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+		},
+		tags = { "europe", "goods", "waggon" },
+		minGameVersion = 0,
+		dependencies = { "mknj_container_asset_set_1", "modwerkstatt_zugschluss_1", "bandion_pausbacke_1", "modwerkstatt_mwagen_1", },
+		url = { "" },
+	},
+	options = {
+	},
+	
+	runFn = function (settings)
+	
+	end
+	}
+end
---
+++ strings.lua
@@ -0,0 +1,72 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "RoRo \"Rolling Road\" (Saadkms)",
+		["mod_desc"] = "The Rolling Road (RoRo) is a transport system for accompanied combined transport by rail. It is a train in which complete lorries or articulated lorries are transported by rail. The short-coupled low-floor wagons with small wheel diameters used for this purpose have continuous lanes running along the entire train. During the journey, the drivers are accommodated in additionally attached escort wagons (seating or couchette wagons).\n\nThe following mods are required: train end, cargo asset, m-wagon, MAN chubby jaws",
+
+        ["RoLa_menu"] = "RoRo \"Rolling Road\"",
+        ["Saadkms_Back_DB_name"] = "Saadkms 690 DB (back)",
+		["Saadkms_Back_DBAG_name"] = "Saadkms 690 DB AG (back)",
+		["Saadkms_Back_OEBB_Oekombi_name"] = "Saadkms 690 ÖBB Ökombi (back)",
+		["Saadkms_Back_OEBB_name"] = "Saadkms 690 ÖBB (back)",
+		["Saadkms_Back_RAlpin_name"] = "Saadkms 690 RAlpin (back)",
+		["Saadkms_Back_Hupac_name"] = "Saadkms 690 Hupac (back)",
+		["Saadkms_Back_desc"] = "The rear end is fixed, the front and rear end are variable. To be used as a wagon at the end of the RoMo, if something is still coupled at the rear.",
+		["Saadkms_Front_DB_name"] = "Saadkms 690 DB (front)",
+        ["Saadkms_Front_DBAG_name"] = "Saadkms 690 DB AG (front)",
+        ["Saadkms_Front_OEBB_Oekombi_name"] = "Saadkms 690 ÖBB Ökombi (front)",
+        ["Saadkms_Front_OEBB_name"] = "Saadkms 690 ÖBB (front)",
+        ["Saadkms_Front_RAlpin_name"] = "Saadkms 690 RAlpin (front)",
+        ["Saadkms_Front_Hupac_name"] = "Saadkms 690 Hupac (front)",
+		["Saadkms_Front_desc"] = "Front end is fixed, rear end and rear end are variable. To be used as start of the RoLa behind locomotive or other wagons.",
+		["Saadkms_DB_name"] = "Saadkms 690 DB",
+        ["Saadkms_DBAG_name"] = "Saadkms 690 DB AG",
+        ["Saadkms_OEBB_Oekombi_name"] = "Saadkms 690 ÖBB Ökombi",
+        ["Saadkms_OEBB_name"] = "Saadkms 690 ÖBB",
+        ["Saadkms_RAlpin_name"] = "Saadkms 690 RAlpin",
+        ["Saadkms_Hupac_name"] = "Saadkms 690 Hupac",
+		["Saadkms_desc"] = "Train end and head ends variable at the front and rear. To be used as a middle section or optionally as a train end if no more wagons follow.",
+
+        ["Bcm243_name"] = "Bcm 243 DB",
+        ["Bcm255_name"] = "Bcm 255 DB AG",
+        ["Bcmz_Oebb_name"] = "Bcmz ÖBB Ökombi",
+        ["RoLaBegleitwagen_desc"] = "The trucks stand on the flat cars of the RoRo while the drivers are accommodated in an escort car. These are couchette cars and offer space to rest and recover during the journey.",
+
+	},
+	de = {
+
+		["mod_name"] = "Rollende Landstraße RoLa (Saadkms)",
+		["mod_desc"] = "Die Rollende Landstraße (kurz RoLa) ist ein Transportsystem für den begleiteten kombinierten Verkehr auf der Schiene. Es ist ein Zug bei dem komplette Lastwagen bzw. Sattelzüge per Bahn befördert werden. Die dazu verwendeten kurzgekuppelten Niederflurwagen mit kleinen Raddurchmessern haben durchgängig über den ganzen Zug hinweglaufende Fahrspuren. Die Fahrer sind während der Fahrt in zusätzlich angehängten Begleitwagen (Sitz- oder Liegewagen) untergebracht. \n\nBenötigt werden folgende Mods: Zugschluß, Cargo Asset, m-Wagen, MAN Pausbacke",
+
+        ["RoLa_menu"] = "Rollende Landstraße",
+        ["Saadkms_Back_DB_name"] = "Saadkms 690 DB (hinten)",
+		["Saadkms_Back_DBAG_name"] = "Saadkms 690 DB AG (hinten)",
+		["Saadkms_Back_OEBB_Oekombi_name"] = "Saadkms 690 ÖBB Ökombi (hinten)",
+		["Saadkms_Back_OEBB_name"] = "Saadkms 690 ÖBB (hinten)",
+		["Saadkms_Back_RAlpin_name"] = "Saadkms 690 RAlpin (hinten)",
+		["Saadkms_Back_Hupac_name"] = "Saadkms 690 Hupac (hinten)",
+		["Saadkms_Back_desc"] = "Kopfstück hinten ist fest montiert, Zugschluß und Kopfstück vorne variabel. Als Wagen am Ende der RoLa einzusetzen, wenn hinten dran noch etwas gekuppelt wird.",
+		["Saadkms_Front_DB_name"] = "Saadkms 690 DB (vorne)",
+        ["Saadkms_Front_DBAG_name"] = "Saadkms 690 DB AG (vorne)",
+        ["Saadkms_Front_OEBB_Oekombi_name"] = "Saadkms 690 ÖBB Ökombi (vorne)",
+        ["Saadkms_Front_OEBB_name"] = "Saadkms 690 ÖBB (vorne)",
+        ["Saadkms_Front_RAlpin_name"] = "Saadkms 690 RAlpin (vorne)",
+        ["Saadkms_Front_Hupac_name"] = "Saadkms 690 Hupac (vorne)",
+		["Saadkms_Front_desc"] = "Kopfstück vorne ist fest montiert, Zugschluß und Kopfstück hinten variabel. Als Start der RoLa einzusetzen hinter Lokomotive oder anderen Wagen.",
+		["Saadkms_DB_name"] = "Saadkms 690 DB",
+        ["Saadkms_DBAG_name"] = "Saadkms 690 DB AG",
+        ["Saadkms_OEBB_Oekombi_name"] = "Saadkms 690 ÖBB Ökombi",
+        ["Saadkms_OEBB_name"] = "Saadkms 690 ÖBB",
+        ["Saadkms_RAlpin_name"] = "Saadkms 690 RAlpin",
+        ["Saadkms_Hupac_name"] = "Saadkms 690 Hupac",
+		["Saadkms_desc"] = "Zugschluß und Kopfstücke vorne und hinten variabel. Als Mittelstück oder wahlweise als Zugschluß einzusetzen, wenn keine weiteren Wagen mehr folgen.",
+
+        ["Bcm243_name"] = "Bcm 243 DB",
+        ["Bcm255_name"] = "Bcm 255 DB AG",
+        ["Bcmz_Oebb_name"] = "Bcmz ÖBB Ökombi",
+        ["RoLaBegleitwagen_desc"] = "Auf den Flachwagen der RoLa stehen die LKW während die Fahrer in einem Begleitwagen untergebracht werden. Dies sind Liegewagen und bieten Raum zum Ausruhen und Erholung während der Fahrt.",
+
+	},
+}
+end
